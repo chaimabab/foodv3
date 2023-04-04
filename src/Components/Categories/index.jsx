cbd3f3f8167f2b47914fe923b7f8f41ea5f326c9
“elayeboussama” <61306430+elayeboussama@users.noreply.github.com>
2023-04-04 07:22:08 +0100

    update

--- src/Components/Categories/index.jsx
+++ src/Components/Categories/index.jsx
@@ -224,181 +224,185 @@ const cardWidth = width/2 -20;
 const Categories = () => {
   const [modalState ,setModalState] = React.useState(0)
   const [category, setCategory]=React.useState();
-  const [categoryProds, setCategoryProds]=React.useState([{
-    id: '1',
-    category: "Pizza",
+  const [update, setUpdate] = React.useState(false);
+  const [categoryProds, setCategoryProds] = React.useState([
+    {
+      id: "1",
+      category: "Pizza",
 
-    name: 'Meat Pizza',
-    ingredients: 'Mixed Pizza',
-    price: '12.00',
-    image: require('../../assets/meatPizza.png'),
-},
-{
-    category: "Pizza",
-    id: '2',
-    name: 'Cheese Pizza',
-    ingredients: 'Cheese Pizza',
-    price: '16.00',
-    image: require('../../assets/cheesePizza.png'),
-},
-{
-    category: "Pizza",
-    id: '3',
-    name: 'Pepperoni Pizza',
-    ingredients: 'Fried Chicken',
-    price: '18.00',
-    image: require('../../assets/pepperoniPizza.png'),
-}]);
-  const [open, setOpen]=React.useState(false);
+      name: "Meat Pizza",
+      ingredients: "Mixed Pizza",
+      price: "12.00",
+      image: require("../../assets/meatPizza.png"),
+    },
+    {
+      category: "Pizza",
+      id: "2",
+      name: "Cheese Pizza",
+      ingredients: "Cheese Pizza",
+      price: "16.00",
+      image: require("../../assets/cheesePizza.png"),
+    },
+    {
+      category: "Pizza",
+      id: "3",
+      name: "Pepperoni Pizza",
+      ingredients: "Fried Chicken",
+      price: "18.00",
+      image: require("../../assets/pepperoniPizza.png"),
+    },
+  ]);
+  const [open, setOpen] = React.useState(false);
   //const [data, setData]=React.useState([...foods, ...Burgers, ...Sandwichs]);
-  const [cats, setCats]=React.useState();
+  const [cats, setCats] = React.useState();
 
-  useEffect(()=>{  
-     handleData()
-     handleProdsByCat(category)
-    console.log(cats); 
-  },[cats])
+  useEffect(() => {
+    handleData();
+    handleProdsByCat(category);
+    console.log(cats);
+  }, [cats]);
 
-  const handleData =  () =>{
-    axios.get("http://192.168.1.15/admin/api/categories")
-    .then((response) => {
-      setCats(response.data)
-      setCategory(response.data[0].id)
-    }).catch((err)=>{
-    console.log(err)
-  })
-  }
+  const handleData = () => {
+    axios
+      .get("http://192.168.1.15/admin/api/categories")
+      .then((response) => {
+        setCats(response.data);
+        setCategory(response.data[0].id);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
+  useEffect(() => {
+    handleProdsByCat(category);
+    console.log(category);
+  }, [, category]);
 
-  useEffect(()=>{
-     handleProdsByCat(category)
-    console.log(category)
-  },[, category])
+  const handleProdsByCat = (id) => {
+    axios
+      .get(`http://192.168.1.15/admin/api/categories/products/${id}`)
+      .then((response) => {
+        setCategoryProds(response.data);
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
+  const [stateModalTicket, setStateModalTicket] = React.useState(false);
+  const [stateModalTable, setStateModalTable] = React.useState(false);
+  const [stateModalRevenus, setStateModalRevenus] = React.useState(false);
+  const [stateModalClotureZ, setStateModalClotureZ] = React.useState(false);
+  const [stateModalIndex, setStateModalIndex] = React.useState(0);
 
-  const handleProdsByCat =  (id) =>{
-    axios.get(`http://192.168.1.15/admin/api/categories/products/${id}`)
-    .then((response) => {
-        setCategoryProds(response.data)
-        console.log(response.data)
-    }).catch((err)=>{
-    console.log(err)
-  })
-  }
-
-  const [stateModalTicket, setStateModalTicket] = React.useState(false)
-  const [stateModalTable, setStateModalTable] = React.useState(false)
-  const [stateModalRevenus, setStateModalRevenus] = React.useState(false)
-  const [stateModalClotureZ, setStateModalClotureZ] = React.useState(false)
-  const [stateModalIndex, setStateModalIndex] = React.useState(0)
-
-  useEffect(()=>{
-      console.log(stateModalIndex)
-      if(stateModalIndex==1){
-        console.log( "Ticket")
-        setStateModalTicket(true)
-      } 
-      if(stateModalIndex==2){
-        console.log("Table")
-        setStateModalTable(true);
-      }
-      if(stateModalIndex==3){
-        console.log("Revenus")
-
-        setStateModalRevenus(true);
-      }
-      if(stateModalIndex==4){
-        console.log("ClotureZ")
-
-        setStateModalClotureZ(true);
-      }
-  },[stateModalIndex])
-  
-  return (
-    <View style={styles.Categories}> 
-      {open ? 
-          <Animatable.View
-          animation="bounceInLeft"
-          duration={600} 
-          style={styles.drawer}>
-            <AntDesign
-                  onPress={() => setOpen(false) }
-                  name="menu-unfold"
-                  size={40}
-                  color="#DF0F0F"
-                  style={{margin:10 }}
-              />
-            <ListCategoriesVertical setStateModalIndex={setStateModalIndex} /> 
-          </Animatable.View>  
-
-          :""
-    
+  useEffect(() => {
+    console.log(stateModalIndex);
+    if (stateModalIndex == 1) {
+      console.log("Ticket");
+      setStateModalTicket(true);
     }
+    if (stateModalIndex == 2) {
+      console.log("Table");
+      setStateModalTable(true);
+    }
+    if (stateModalIndex == 3) {
+      console.log("Revenus");
+
+      setStateModalRevenus(true);
+    }
+    if (stateModalIndex == 4) {
+      console.log("ClotureZ");
+
+      setStateModalClotureZ(true);
+    }
+  }, [stateModalIndex]);
+
+  return (
+    <View style={styles.Categories}>
+      {open ? (
+        <Animatable.View
+          animation="bounceInLeft"
+          duration={600}
+          style={styles.drawer}
+        >
+          <AntDesign
+            onPress={() => setOpen(false)}
+            name="menu-unfold"
+            size={40}
+            color="#DF0F0F"
+            style={{ margin: 10 }}
+          />
+          <ListCategoriesVertical setStateModalIndex={setStateModalIndex} />
+        </Animatable.View>
+      ) : (
+        ""
+      )}
       <View style={styles.cat}>
         <AntDesign
-              onPress={() => setOpen(true) }
-              name="menu-fold"
-              size={40}
-              color="#DF0F0F"
-              style={{marginLeft:25}}
-          />
-        {cats? 
-          <ListCategories setCategory={setCategory} cats={cats}  /> 
-          :""  
-      }
+          onPress={() => setOpen(true)}
+          name="menu-fold"
+          size={40}
+          color="#DF0F0F"
+          style={{ marginLeft: 25 }}
+        />
+        {cats ? <ListCategories setCategory={setCategory} cats={cats} /> : ""}
       </View>
-      
+
       <View style={styles.souscat}>
         <FlatList
-        contentContainerStyle = {
-          {width:"80%", height:"auto", alignSelf:"center" } }
-        style={styles.souscatList}
-        showsVerticalScrollIndicator={false}
-        numColumns={4}
-        data={categoryProds&&category?categoryProds:categoryProds}
-        renderItem={({item} ) => <Card food ={item} /> }/> 
+          contentContainerStyle={{
+            width: "80%",
+            height: "auto",
+            alignSelf: "center",
+          }}
+          style={styles.souscatList}
+          showsVerticalScrollIndicator={false}
+          numColumns={4}
+          data={categoryProds && category ? categoryProds : categoryProds}
+          renderItem={({ item }) => (
+            <Card food={item} update={update} setUpdate={setUpdate} />
+          )}
+        />
+      </View>
 
-      </View>  
+      {stateModalIndex == 1 ? (
+        <TicketModal
+          stateModalTicket={stateModalTicket}
+          setStateModalTicket={setStateModalTicket}
+        />
+      ) : (
+        ""
+      )}
 
-      {stateModalIndex==1 ?
-        
-              <TicketModal stateModalTicket={stateModalTicket} setStateModalTicket={setStateModalTicket}/>
-            
-      :
-            ""
-    }
+      {stateModalIndex == 2 ? (
+        <TableModal
+          stateModalTable={stateModalTable}
+          setStateModalTable={setStateModalTable}
+        />
+      ) : (
+        ""
+      )}
 
-      {stateModalIndex==2 ?
-              
-              <TableModal stateModalTable={stateModalTable} setStateModalTable={setStateModalTable}/>
-              
-            
-      :
-            ""
-      }
+      {stateModalIndex == 3 ? (
+        <RevenusModal
+          stateModalRevenus={stateModalRevenus}
+          setStateModalRevenus={setStateModalRevenus}
+        />
+      ) : (
+        ""
+      )}
 
-      {stateModalIndex==3 ?
-              
-              <RevenusModal stateModalRevenus={stateModalRevenus} setStateModalRevenus={setStateModalRevenus}/>
-              
-            
-      :
-            ""
-      }
-
-
-      {stateModalIndex==4 ?
-              
-              <ClotureZModal stateModalClotureZ={stateModalClotureZ} setStateModalClotureZ={setStateModalClotureZ}/>  
-            
-      :
-            ""
-      }   
-
-
+      {stateModalIndex == 4 ? (
+        <ClotureZModal
+          stateModalClotureZ={stateModalClotureZ}
+          setStateModalClotureZ={setStateModalClotureZ}
+        />
+      ) : (
+        ""
+      )}
     </View>
-
-    
   );
 };
 const style = StyleSheet.create({
